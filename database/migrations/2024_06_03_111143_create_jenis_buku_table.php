15c43efb2b8c45a7c807f9532294814307dbee27
Ahmad Zaki <125362893+ahmdzzzki@users.noreply.github.com>
2024-06-03 21:05:27 +0700

    migration database

---
+++ database/migrations/2024_06_03_111143_create_jenis_buku_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jenis_buku', function (Blueprint $table) {
+            $table->increments('id_jenis_buku');
+            $table->string('nama_jenis_buku');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jenis_bukus');
+    }
+};

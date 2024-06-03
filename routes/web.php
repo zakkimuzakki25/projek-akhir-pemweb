355cd87ee3710c005eb1125e0d8e3e6b9177487a
zakki <zakkimuzakki050@gmail.com>
2024-06-03 21:08:23 +0700

    add route fav

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages/home');
 });
+
+Route::get('/profil', function () {
+    return view('pages/profile');
+});
+Route::get('/profil/favorit', function () {
+    return view('pages/favourite');
+});
